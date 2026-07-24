3f962045e0f32b06eacba490f28992006798f4fb
RK <Raj.k@betaara.com>
2026-07-24 11:34:41 +0530

    Initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Layout } from './layout/layout';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: Layout,
+        loadChildren: () => import('./layout/layout.routes').then(m => m.layOutRoutes)
+    },
+];
---
+++ src/app/layout/layout.routes.ts
@@ -0,0 +1,10 @@
+import { Routes } from '@angular/router';
+import { Home } from '../component/home/home';
+
+export const layOutRoutes: Routes = [
+    {
+        path: '',
+        component: Home
+    }
+
+];
