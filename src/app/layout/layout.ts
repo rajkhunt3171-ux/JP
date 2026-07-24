2eb2a4b13514dbeadcca7e533600120992f0cecc
RK <Raj.k@betaara.com>
2026-07-24 14:43:23 +0530

    implement layout component and header with global styling configuration

--- src/app/layout/layout.ts
+++ src/app/layout/layout.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from "../shared/component/header/header";
+import { Footer } from "../shared/component/footer/footer";
 
 @Component({
   selector: 'app-layout',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, Footer],
   templateUrl: './layout.html',
   styleUrl: './layout.scss',
 })
