f5cac29c3d715ebea9c8990a17a98c35bb835b0b
RK <Raj.k@betaara.com>
2026-07-25 14:20:03 +0530

    feat: implement home page swiper banner and shared footer component with Swiper integration

--- src/app/component/home/home.ts
+++ src/app/component/home/home.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
+import Swiper from 'swiper/bundle';
 
 @Component({
   selector: 'app-home',
@@ -6,4 +7,42 @@ import { Component } from '@angular/core';
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
-export class Home {}
+export class Home implements AfterViewInit {
+  @ViewChild('swiper') swiperEl!: ElementRef;
+  swiperInstance?: Swiper;
+
+  ngAfterViewInit() {
+    this.swiperInstance = new Swiper(this.swiperEl.nativeElement, {
+      slidesPerView: 1,
+      // spaceBetween: 20,
+      loop: true,
+
+      autoplay: {
+        delay: 3000,
+        disableOnInteraction: false
+      },
+
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true
+      },
+
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
+      },
+
+      breakpoints: {
+        320: {
+          slidesPerView: 1
+        },
+        768: {
+          slidesPerView: 1
+        },
+        1200: {
+          slidesPerView: 1
+        }
+      }
+    });
+  }
+}
