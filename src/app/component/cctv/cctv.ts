38463ec3c396ffc01a330057772eed07efa04ba6
RK <Raj.k@betaara.com>
2026-08-05 11:37:13 +0530

    feat: add CCTV product component and styling updates to home page

--- src/app/component/cctv/cctv.ts
+++ src/app/component/cctv/cctv.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { ProductDetails } from '../product-details/product-details';
 
 @Component({
   selector: 'app-cctv',
@@ -6,4 +8,48 @@ import { Component } from '@angular/core';
   templateUrl: './cctv.html',
   styleUrl: './cctv.scss',
 })
-export class Cctv {}
+export class Cctv {
+  productList: any[] = [
+    {
+      img: 'dahua_dome_bullet_camera.jpg',
+      title: 'Dahua Dome & Bullet CCTV Security Camera Series',
+      description: 'Dahua Dome and Bullet CCTV cameras provide reliable indoor and outdoor surveillance solutions for homes, offices, and commercial spaces. Dome cameras are designed for compact, vandal-resistant indoor or ceiling-mounted use, while Bullet cameras excel in long-range outdoor monitoring with weatherproof protection (IP67).',
+      short_desc: [
+        'HD & IP High Resolution Options (2MP, 4MP, 5MP to 8MP/4K)',
+        'Night Vision & WizSense Full-Color Technology for clear night video',
+        'Built-in Audio Microphone for audio recording support',
+        'IP67 Weatherproof and IK10 Vandal-Resistant Housing',
+        'Smart Motion Detection (SMD) & AI Intrusion Alerts'
+      ],
+      other_desc: [
+        'Dome Cameras: Ideal for ceiling mounting indoors with a discreet look and impact protection.',
+        'Bullet Cameras: Best for outdoor walls, perimeters, and long-distance night vision monitoring.',
+        'Dual Technology Support: Available in both HDCVI (Analog) and IP/Network configurations.',
+        'H.265+ Compression: Maximizes DVR/NVR storage efficiency and reduces bandwidth consumption.',
+        'Remote Access: Supports live streaming and smart notifications via the DMSS mobile app.'
+      ]
+    },
+    // {
+    //   img: '',
+    //   title: '',
+    //   description: '',
+    //   short_desc: [],
+    //   other_desc: []
+    // },
+  ]
+
+  constructor(
+    private modalService: NgbModal
+  ) { }
+
+  productDetails(produtData: any) {
+    console.log(produtData);
+
+    const modalRef = this.modalService.open(ProductDetails, {
+      size: 'xl',
+      backdrop: true,
+      centered: true,
+    });
+    modalRef.componentInstance.productData = produtData;
+  }
+}
---
+++ src/app/component/product-details/product-details.ts
@@ -0,0 +1,28 @@
+import { Component, Input } from '@angular/core';
+import { Service } from '../../shared/service/service';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+  selector: 'app-product-details',
+  imports: [],
+  templateUrl: './product-details.html',
+  styleUrl: './product-details.scss',
+})
+export class ProductDetails {
+  @Input() productData: any;
+
+  constructor(
+    public sharedService: Service,
+    public activeModal: NgbActiveModal
+  ) { }
+
+  ngOnInit() { }
+
+  redirectToWhatsApp(productName: any) {
+    const phoneNumber = '+919429351046';
+    const message = `Hi,\nWe need more information about your product ${productName}.`;
+    const encodedMessage = encodeURIComponent(message);
+    const whatsappUrl = `https://wa.me/${phoneNumber}?text=${encodedMessage}`;
+    window.open(whatsappUrl, '_blank');
+  }
+}
