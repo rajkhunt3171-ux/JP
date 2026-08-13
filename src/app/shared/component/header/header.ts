5a84af3ebb050b97f44a53121b7630f9e991e115
raj-khunt <rajkh@keypress.co.in>
2026-08-13 21:00:48 +0530

    feat: add responsive header component with mobile sidebar toggle functionality

--- src/app/shared/component/header/header.ts
+++ src/app/shared/component/header/header.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Service } from '../../service/service';
 import { SideBar } from "../side-bar/side-bar";
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-header',
-  imports: [SideBar],
+  imports: [SideBar, RouterLink],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
